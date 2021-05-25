424c453fad423089b7d00d8ef5b382c7cb0d0862
sung-ilkim <skim88.1942@gmail.com>
2021-05-25 22:27:39 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from "react";
 import CounterContainer from "./containers/CounterContainer";
 import TodosContainer from "./containers/TodosContainer";
-import { Container } from '@material-ui/core';
+import { Card } from '@material-ui/core';
 
 function App() {
   return (
-    <div class="App" style={{height: "100%", overflow: "hidden"}}>
-      <Container style={{height: "100%"}}>
+    <div class="App" style={{height: "100%", overflow: "hidden", backgroundColor: "lightgreen"}}>
+      <Card style={{marginLeft: "5%", marginRight: "5%", marginTop: "5%", marginBottom: "5%", height: "90%"}}>
         <CounterContainer />
         <hr />
         <TodosContainer />
-      </Container>
+      </Card>
     </div>
   );
 }
